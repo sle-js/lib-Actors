c8ff49fb097008423ee9df28cd43d5e7b93741c1
Graeme Lockley <graemel@no9.co.za>
2018-06-02 11:39:06 +0200

    feature: add the initial Actors implementation

--- index.js
+++ index.js
@@ -1,5 +1,131 @@
 module.exports = $importAll([
-
+    "util"
 ]).then($imports => {
+    const Util =
+        $imports[0];
 
+
+    let actorIDCount =
+        0;
+
+
+    // type Msg = None | Single ActorID Any | Batch (List Msg)
+
+    const None =
+        [0];
+
+    const Single = actorID => value =>
+        [1, actorID, value];
+
+    const Batch = msgs =>
+        [2, msgs];
+
+
+    const deliver = msg => {
+        switch (msg[0]) {
+            case 0:
+                break;
+
+            case 1:
+                msg[1].process(msg[2]);
+                break;
+
+            case 2:
+                for (let item of msg[1]) {
+                    deliver(item);
+                }
+                break;
+
+        }
+    };
+
+
+    const Msg = {
+        isNone: msg => msg[0] === 0,
+        isSingle: msg => msg[0] === 1,
+        singleActorID: msg => msg[1].id(),
+        singleValue: msg => msg[2],
+        isBatch: msg => msg[0] === 2,
+        batchItems: msg => msg[1],
+        deliver: deliver
+    };
+
+
+    // Actor : (Self -> State) -> (Self -> Msg -> State -> (State, Task)) -> Self
+    function Actor(init, update) {
+        this._id = actorIDCount++;
+        this._state = Promise.resolve(undefined);
+        this._update = update;
+
+        const self =
+            this;
+
+        this._state = this._state
+            .then(s => {
+                try {
+                    const result =
+                        init(self);
+
+                    deliver(result[1]);
+
+                    return result[0];
+                } catch (e) {
+                    console.error(`${new Date()}: ${this.id}: Exception thrown: ${Util.inspect({
+                        e: e.message,
+                        stackTrace: e.stack
+                    }, {depth: 3})}`);
+
+                    return s;
+                }
+            });
+    }
+
+
+    Actor.prototype.id = function() {
+        return this._id.toString();
+    };
+
+
+    Actor.prototype.send = function (value) {
+        return Single(this)(value);
+    };
+
+
+    Actor.prototype.process = function (value) {
+        const self =
+            this;
+
+        this._state = this._state
+            .then(s => {
+                try {
+                    const result =
+                        self._update(self, value, s);
+
+                    deliver(result[1]);
+
+                    return result[0];
+                } catch (e) {
+                    console.error(`${new Date()}: ${this.id}: Exception thrown: ${Util.inspect({
+                        state: s,
+                        value: value,
+                        e: e.message,
+                        stackTrace: e.stack
+                    }, {depth: 3})}`);
+
+                    return s;
+                }
+            });
+    };
+
+
+    const create = (update, init) =>
+        new Actor(init ? init : self => [0, None], update);
+
+
+    return {
+        create,
+        None,
+        Batch,
+        Msg
+    };
 });
--- test/ActorsTest.js
+++ test/ActorsTest.js
@@ -1,8 +1,27 @@
 module.exports = $import(
     "./Libs"
 ).then($imports => {
+    const Actors =
+        $imports.Index;
+
+    const Assertion =
+        $imports.Assertion;
+
     const Unit =
         $imports.Unit;
 
-    return Unit.Suite("Actors")([]);
+    return Unit.Suite("Create Actor")([(() => {
+        const echoActor =
+            Actors.create((self, msg, state) => [msg, Actors.None]);
+
+        const msg =
+            echoActor.send("hello");
+
+        return Unit.Test("echo")(
+            Assertion
+                .equals(Actors.Msg.isSingle(msg))(true)
+                .equals(Actors.Msg.singleValue(msg))("hello")
+        )
+    })()
+    ]);
 });
